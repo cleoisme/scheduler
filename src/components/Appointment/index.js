7fbf9b79ec08635b34461d710828aca3b0507368
Cleo Zhang <forest-zhang@hotmail.com>
2019-10-13 15:11:58 -0700

    Add Header & Empty Components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,5 @@
 import "./style.scss";
+import React from "react";
 
 export default function Appointment(props) {
   return <article className="appointment"></article>;
--- stories/index.js
+++ stories/index.js
@@ -6,12 +6,14 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+
 import Button from "components/Button";
 import DayList from "components/DayList";
 import DayListItem from "components/DayListItem";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-
 const interviewer = {
   id: 1,
   name: "Sylvia Palmer",
@@ -133,4 +135,6 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
